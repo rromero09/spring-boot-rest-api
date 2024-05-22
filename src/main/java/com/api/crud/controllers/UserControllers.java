c59cb4700ef8f5e11a6dd77011371bd3d3b2a68b
rromero09 <rromero8@neiu.edu>
2024-05-22 16:49:03 -0500

    initial commit

---
+++ src/main/java/com/api/crud/controllers/UserControllers.java
@@ -0,0 +1,28 @@
+package com.api.crud.controllers;
+
+import com.api.crud.models.UserModel;
+import com.api.crud.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/user")
+public class UserControllers {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ArrayList<UserModel> getUsers(){
+        return this.userService.getUsers();
+    }
+
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user) {
+        return this.userService.saveUser(user);
+    }
+
+
+}
---
+++ src/main/java/com/api/crud/services/UserService.java
@@ -0,0 +1,28 @@
+package com.api.crud.services;
+
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    IUserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers(){
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel user){
+        return userRepository.save(user);
+    }
+
+    public Optional<UserModel> getById(Long id){
+        return userRepository.findById(id);
+    }
+}
